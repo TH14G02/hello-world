95ba75bb3b448f19ac3338225fda8fa5f0a29a55
Maykol <maykolsampaio@ifpi.edu.br>
2025-10-24 11:38:51 -0300

    ajuste index.js

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,10 +1,3 @@
-document.getElementById('imc-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const weight = parseFloat(document.getElementById('weight').value).toFixed(1);
-    const height = parseFloat(document.getElementById('height').value).toFixed(1);
-    window.location.href = `/imc/${weight}/${height}`;
-});
-
 document.getElementById('calc-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const a = document.getElementById('a').value;
